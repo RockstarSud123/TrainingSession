e9ca7be114e66fe59df785fde33768ad298f2a5b
Rockstarsud <sudrockstar123@gmail.com>
2019-03-27 09:48:35 +0530

    Functionality required for searching has been implemented successfully.

--- SearchBox/script.js
+++ SearchBox/script.js
@@ -1,26 +1,40 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-  
-function filterFunction() {
+var states = new Array();
 
+states['India'] = new Array('Andhra Pradesh','Arunachal Pradesh','Assam','Bihar','Chhattisgarh','Goa','Gujarat','Haryana','Himachal Pradesh','Jammu and Kashmir','Jharkhand','Karnataka','Kerala','Madhya Pradesh','Maharashtra','Manipur','Meghalaya','Mizoram','Nagaland','Odisha','Punjab','Rajasthan','Sikkim','Tamil Nadu','Telangana','Tripura','Uttar Pradesh','Uttarakhand','WestBengal','Andaman and Nicobar Islands','Chandigarh','Dadra and Nagar Haveli','Daman and Diu','Lakshadweep','Puducherry'); 
+
+var list = states['India'];  
+
+function filterFunction() {
+    
+    var arr = new Array();
+    
+    var ulist = document.getElementById("stateList");   
     var input = document.getElementById("myInput").value;
     var filter = input.toUpperCase();
-    var a = document.getElementsByTagName("li");
+    
+    for (var i = 0; i < list.length; i++) {  
 
-    for (var i = 0; i < a.length; i++) {  
-        var txtValue = a[i].textContent;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        var txtValue = list[i];
+   
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {       
 
-            console.log(txtValue.toUpperCase());
-            console.log(filter);
-            console.log(txtValue.toUpperCase().indexOf(filter));
+            arr.push(txtValue);
+             
+            arr.sort();
 
-            a[i].style.display = "";
-        } else {
-            a[i].style.display = "none";
+            document.getElementById("stateList").innerHTML = "";
+        }
+        else{
+            document.getElementById("stateList").innerHTML = ""; 
         }
     }
-  
+
+    for(var i=0; i<arr.length; i++){
+        var li = document.createElement("li"); 
+        li.appendChild(document.createTextNode(arr[i]));
+        ulist.appendChild(li);
+    }
 }
+
+
 
