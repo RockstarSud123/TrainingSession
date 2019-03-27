c6ff576b051aa6dfae456e99784878880fd3408e
Rockstarsud <sudrockstar123@gmail.com>
2019-03-27 11:22:13 +0530

    unwanted steps were removed from the file.

--- TimeAndDate/script.js
+++ TimeAndDate/script.js
@@ -9,50 +9,22 @@ var dday = day+10;
 
 if(day==1 || day==21 || day==31){
 
-    console.log("inside if 1");
-
     day += "st".sup();
-
-    console.log(day);
-    console.log(month);
-    console.log(year);
-    
     document.getElementById("date").innerHTML = day + " " + month + ", " + year;
 }
 else if(day==2 || day==22){
 
-    console.log("inside else if 1");
-
     day += "nd".sup();
-
-    console.log(day);
-    console.log(month);
-    console.log(year);
-    
     document.getElementById("date").innerHTML = day + " " + month + ", " + year;
 }
 else if(day==3 || day==23){
 
-    console.log("inside else if 2");
-
     day += "rd".sup();
-
-    console.log(day);
-    console.log(month);
-    console.log(year);
-    
     document.getElementById("date").innerHTML = day + " " + month + ", " + year;
 }
 else{
 
-    console.log("inside else 1");
-
     day += "th".sup();
-
-    console.log(day);
-    console.log(month);
-    console.log(year);
-    
     document.getElementById("date").innerHTML = day + " " + month + ", " + year;
 }
 
@@ -76,19 +48,14 @@ function timeFunction(){
     }
 
     if(hour > 12){
-
-        console.log("inside if 3");
-
+     
         hour = hour-12;
         hour = checkTime(hour);
         document.getElementById("time").innerHTML = hour + ":" + minute + ":" + second + " " + "PM";                     
     }
     else{
-
-        console.log("inside else 3");
-
+    
         document.getElementById("time").innerHTML = hour + ":" + minute + ":" + second + " " + "AM";
     }
     var variable = setTimeout(timeFunction, 1000);
-    // console.log(variable);
 }
