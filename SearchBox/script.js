8342a8def89a0c3f00e7430f45762c4746e1c864
Rockstarsud <sudrockstar123@gmail.com>
2019-04-02 15:06:20 +0530

    Updation to the code was done successfully.

--- SearchBox/script.js
+++ SearchBox/script.js
@@ -1,43 +1,34 @@
-var states = new Array();
-
-states['India'] = new Array('Andhra Pradesh','Arunachal Pradesh','Assam','Bihar','Chhattisgarh','Goa','Gujarat','Haryana','Himachal Pradesh','Jammu and Kashmir','Jharkhand','Karnataka','Kerala','Madhya Pradesh','Maharashtra','Manipur','Meghalaya','Mizoram','Nagaland','Odisha','Punjab','Rajasthan','Sikkim','Tamil Nadu','Telangana','Tripura','Uttar Pradesh','Uttarakhand','WestBengal','Andaman and Nicobar Islands','Chandigarh','Dadra and Nagar Haveli','Daman and Diu','Lakshadweep','Puducherry'); 
-
-var list = states['India'];  
+var states = ['Andhra Pradesh','Arunachal Pradesh','Assam','Bihar','Chhattisgarh','Goa','Gujarat','Haryana','Himachal Pradesh','Jammu and Kashmir','Jharkhand','Karnataka','Kerala','Madhya Pradesh','Maharashtra','Manipur','Meghalaya','Mizoram','Nagaland','Odisha','Punjab','Rajasthan','Sikkim','Tamil Nadu','Telangana','Tripura','Uttar Pradesh','Uttarakhand','WestBengal','Andaman and Nicobar Islands','Chandigarh','Dadra and Nagar Haveli','Daman and Diu','Lakshadweep','Puducherry']; 
+var list = states;  
 
 function filterFunction() {
     
-    var arr = new Array();
-    
-    var ulist = document.getElementById("stateList");   
+    var arr = [];
+    var ul = document.getElementById("stateList");   
     var input = document.getElementById("myInput").value;
     var filter = input.toUpperCase();
-    
+
     for (var i = 0; i < list.length; i++) {  
 
         var txtValue = list[i];
-   
+
         if (txtValue.toUpperCase().indexOf(filter) > -1) {       
 
             arr.push(txtValue);
-             
             arr.sort();
-
-            document.getElementById("stateList").innerHTML = "";
-         }
-        else if(arr=="")
-        {
-            document.getElementById("stateList").innerHTML = "No results found.";
-            document.getElementById("stateList").style.color="red";
-            document.getElementById("stateList").style.fontSize="2rem";
+            ul.innerHTML = "";
         }
     }
 
-    for(var i=0; i<arr.length; i++){
+    if(arr.length === 0) {
+        ul.innerHTML = "No results found.";
+        ul.style.color="red";
+        ul.style.fontSize="2rem";
+    }
+
+    for(var j=0; j<arr.length; j++){
         var li = document.createElement("li"); 
-        li.appendChild(document.createTextNode(arr[i]));
-        ulist.appendChild(li);
+        li.appendChild(document.createTextNode(arr[j]));
+        ul.appendChild(li);
     }
 }
-
-
-
