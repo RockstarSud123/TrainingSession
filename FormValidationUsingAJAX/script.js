c3181bce29f62e60b0cc2e8454877376d145c05b
Rockstarsud <sudrockstar123@gmail.com>
2019-04-02 15:19:18 +0530

    Unwanted instructions were removed and modifications were done in the code.

--- FormValidationUsingAJAX/script.js
+++ FormValidationUsingAJAX/script.js
@@ -1,18 +1,9 @@
 function validate(){
 
-    validateName();
-    validateEMail();
-    validateGender();
-    validateDateOfBirth();
-    validateMobileNo();
-    validateAddress();
-    validateCity();
-    validateCountry();
-    validateExpertise();
-    validateGroup();
+    var validName = validateName(), validEmail = validateEmail(), validGender = validateGender(), validDOB = validateDOB(), validNumber = validateMobileNo(), validAddress = validateAddress(), validCity = validateCity(), validCountry = validateCountry(), validExpertise = validateExpertise(), validGroup =  validateGroup();
 
-     if(validateName() && validateEMail() && validateGender() && validateDateOfBirth() && validateMobileNo() &&  validateAddress() && validateCity() && validateCountry() && validateExpertise() && validateGroup()){
 
+     if(validName && validEmail && validGender && validDOB && validNumber &&  validAddress && validCity && validCountry && validExpertise && validGroup()){
         // AJAX Request after successfull form validation.
         $.ajax({
             method: "POST",
@@ -45,195 +36,191 @@ function validate(){
      else{
          return false;
      }
-
-
 }
 
 
 function validateName() {
-    var yourName = document.getElementById('yourName').value;
-    console.log(yourName);
-
-    if(yourName==""){
-        document.getElementById('urName').innerHTML="* Please Enter your Full Name.";
+    var fullName = document.getElementById('fullName').value;
+    var domName = document.getElementById('errorName');
+    
+    if(fullName==""){
+        domName.innerHTML="* Please Enter your Full Name.";
         return false;
     }
-    else if(!isNaN(yourName)){
-        document.getElementById('urName').innerHTML="Please enter only character.";
+    else if(!isNaN(fullName)){
+        domName.innerHTML="Please enter only character.";
         return false;
     }
-    else if((yourName.length<4)||(yourName.length>12)){
-        document.getElementById('urName').innerHTML="Characters must be in between 4 to 12.";
+    else if((fullName.length<4)||(fullName.length>12)){
+        domName.innerHTML="Characters must be in between 4 to 12.";
         return false;
     }
     else{
-        document.getElementById('urName').innerHTML="";
+        domName.innerHTML="";
          return true;
     }    
 }
 
-function validateEMail(){
+function validateEmail(){
     var symbols = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    var EMail = document.getElementById('EMail').value;
-    console.log(EMail);
+    var Email = document.getElementById('Email').value;
+    var domEmail = document.getElementById('errorEmail');
 
-    if(EMail==""){
-        document.getElementById('email').innerHTML="Enter Email Address.";
+    if(Email==""){
+        domEmail.innerHTML="Enter Email Address.";
         return false;
     }
-    else if(!EMail.match(symbols)){
-        document.getElementById('email').innerHTML="Please enter valid Email Address.";
+    else if(!Email.match(symbols)){
+        domEmail.innerHTML="Please enter valid Email Address.";
         return false;
     }
     else{
-        document.getElementById('email').innerHTML="";
+        domEmail.innerHTML="";
          return true;
     }
 }
 
 function validateGender(){
-    var gender = document.getElementById('male').value;
-    console.log(gender);
-    if((document.getElementById('male').checked==false) && (document.getElementById('female').checked==false)){
-        document.getElementById('radioButton').innerHTML="Please select your gender.";
+
+    var domMale = document.getElementById('male').checked;
+    var domFemale = document.getElementById('female').checked;
+    var domGender = document.getElementById('errorRadioButton');
+    
+    if((domMale==false) && (domFemale==false)){
+        domGender.innerHTML="Please select your gender.";
         return false;
     }
     else{
-        document.getElementById('radioButton').innerHTML="";
+        domGender.innerHTML="";
          return true; 
     }
 }
 
-function validateDateOfBirth(){
+function validateDOB(){
     symbols=/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
     var dateOfBirth = document.getElementById('dateOfBirth').value;
-    console.log(dateOfBirth);
+    var domDOB = document.getElementById('errorDOB'); 
+
     if(dateOfBirth==""){
-        document.getElementById('dob').innerHTML="Please enter your Date of Birth.";
+        domDOB.innerHTML="Please enter your Date of Birth.";
         return false;
     }
     else if(!dateOfBirth.match(symbols)){
-        document.getElementById('dob').innerHTML="Enter Date of Birth in proper format.";
+        domDOB.innerHTML="Enter Date of Birth in proper format.";
         return false;
     }
     else{
-        document.getElementById('dob').innerHTML="";
+        domDOB.innerHTML="";
           return true;
     }
 }
 
 function validateMobileNo(){
     var mobileNumber = document.getElementById('mobileNumber').value;
-    console.log(mobileNumber);
+    var domNumber = document.getElementById('errorMobileNumber');
+
     if(mobileNumber==""){
-        document.getElementById('mobile').innerHTML="Please enter your contact number.";
+        domNumber.innerHTML="Please enter your contact number.";
         return false;
     }
     else if(isNaN(mobileNumber)){
-        document.getElementById('mobile').innerHTML="Enter only numeric value.";
+        domNumber.innerHTML="Enter only numeric value.";
         return false;
     }
     else if(mobileNumber.length<10||mobileNumber.length>10){
-        document.getElementById('mobile').innerHTML="Contact number must be of 10 digits.";
+        domNumber.innerHTML="Contact number must be of 10 digits.";
         return false;
     }
     else if((mobileNumber.charAt(0)!=9)&&(mobileNumber.charAt(0)!=8)&&(mobileNumber.charAt(0)!=7)){
-        document.getElementById('mobile').innerHTML="Mobile number should always start with 9, 8 or 7.";
+        domNumber.innerHTML="Mobile number should always start with 9, 8 or 7.";
         return false;
     }
     else{
-        document.getElementById('mobile').innerHTML="";
+        domNumber.innerHTML="";
          return true;
     }
 }
 
 function validateAddress(){
     var address=document.getElementById('address').value;
-    console.log(address);
+    var domAddress = document.getElementById('errorAdress');
+
     if(address==""){
-        document.getElementById('adress').innerHTML="Enter your address.";
+        domAddress.innerHTML="Enter your address.";
         return false;
     }    
     else if((address.length<20)||(address.length>100)){
-        document.getElementById('adress').innerHTML="Your textarea must be in between 20 to 100. \n Length of your textarea is : " + address.length;
+        domAddress.innerHTML="Your textarea must be in between 20 to 100. \n Length of your textarea is : " + address.length;
         return false;
     }
     else{
-        document.getElementById('adress').innerHTML="";
+        domAddress.innerHTML="";
          return true;
     }
 }
 
 function validateCity(){
   
-    var cities = document.getElementById('city').value;
-    console.log(cities);
 
-    if(document.getElementById('city').selectedIndex == 0){
-        document.getElementById('citi').innerHTML="Please select your city.";
+    var city = document.getElementById('city').selectedIndex;
+    var domCity = document.getElementById('errorCity');
+
+    if(city == 0){
+        domCity.innerHTML="Please select your city.";
         return false;
     }
     else{
-        document.getElementById('citi').innerHTML="";
+        domCity.innerHTML="";
          return true;
     }
 }
 
 function validateCountry(){
     var country=document.getElementById('country').value;
-    console.log(country);
+    var domCountry = document.getElementById('errorCountry');
+
     if(country==""){
-        document.getElementById('cuntry').innerHTML="Please enter your country.";
+        domCountry.innerHTML="Please enter your country.";
         return false;
     }
     else if(!isNaN(country)){
-        document.getElementById('cuntry').innerHTML="Please enter only character.";
+        domCountry.innerHTML="Please enter only character.";
         return false;
     }
     else if((country.length<4)||(country.length>12)){
-        document.getElementById('cuntry').innerHTML="Characters must be in between 4 to 12.";
+        domCountry.innerHTML="Characters must be in between 4 to 12.";
         return false;
     }
     else{
-        document.getElementById('cuntry').innerHTML="";
+        domCountry.innerHTML="";
          return true;
-    }
+    }  
 }
 
 function validateExpertise(){
-    var checkBox = document.getElementsByName('inlineCheckbox[]');
- 
-    var html = checkBox[0].checked;
-    var css = checkBox[1].checked;
-    var js = checkBox[2].checked;
-    var jQuery = checkBox[3].checked;
-
-    console.log(html);   
-    console.log(css);
-    console.log(js);
-    console.log(jQuery);
+    var checkBox = document.getElementsByName('inlineCheckbox');
+    var domExpertise = document.getElementById('errorExpertise');
 
     if(!checkBox[0].checked && !checkBox[1].checked && !checkBox[2].checked && !checkBox[3].checked){
-        document.getElementById('expertise').innerHTML="Please choose at least one expertise.";
+        domExpertise.innerHTML="Please choose at least one expertise.";
         return false;
     }
     else{
-        document.getElementById('expertise').innerHTML="";
+        domExpertise.innerHTML="";
          return true;
     }
 }
 
 function validateGroup(){
     var group=document.getElementById('group').selectedIndex;
-    var groups=document.getElementById('group').value;
-    console.log(groups);
+    var domGroup = document.getElementById('errorGroup');
 
     if(group == -1){
-        document.getElementById('grup').innerHTML="Please select any one group.";
+        domGroup.innerHTML="Please select any one group.";
         return false;
     }
     else{
-        document.getElementById('grup').innerHTML="";
+        domGroup.innerHTML="";
          return true;
     }
 }
