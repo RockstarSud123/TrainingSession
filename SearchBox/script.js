a1df7e1cd22917ace5bc9a7d3b9b61e7cad405ee
Rockstarsud <sudrockstar123@gmail.com>
2019-04-03 10:41:20 +0530

    Required updation was done in the code successfully.

--- SearchBox/script.js
+++ SearchBox/script.js
@@ -1,12 +1,12 @@
-var states = ['Andhra Pradesh','Arunachal Pradesh','Assam','Bihar','Chhattisgarh','Goa','Gujarat','Haryana','Himachal Pradesh','Jammu and Kashmir','Jharkhand','Karnataka','Kerala','Madhya Pradesh','Maharashtra','Manipur','Meghalaya','Mizoram','Nagaland','Odisha','Punjab','Rajasthan','Sikkim','Tamil Nadu','Telangana','Tripura','Uttar Pradesh','Uttarakhand','WestBengal','Andaman and Nicobar Islands','Chandigarh','Dadra and Nagar Haveli','Daman and Diu','Lakshadweep','Puducherry']; 
-var list = states;  
+var states = ['Andhra Pradesh','Arunachal Pradesh','Assam','Bihar','Chhattisgarh','Goa','Gujarat','Haryana','Himachal Pradesh','Jammu and Kashmir','Jharkhand','Karnataka','Kerala','Madhya Pradesh','Maharashtra','Manipur','Meghalaya','Mizoram','Nagaland','Odisha','Punjab','Rajasthan','Sikkim','Tamil Nadu','Telangana','Tripura','Uttar Pradesh','Uttarakhand','WestBengal','Andaman and Nicobar Islands','Chandigarh','Dadra and Nagar Haveli','Daman and Diu','Lakshadweep','Puducherry'],
+    list = states;  
 
 function filterFunction() {
     
-    var arr = [];
-    var ul = document.getElementById("stateList");   
-    var input = document.getElementById("myInput").value;
-    var filter = input.toUpperCase();
+    var arr = [], 
+        ul = document.getElementById("stateList"), 
+        input = document.getElementById("myInput").value,
+        filter = input.toUpperCase();
 
     for (var i = 0; i < list.length; i++) {  
 
