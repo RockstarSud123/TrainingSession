18b1f97dd865321730224e0e835fc1b84320bf59
Rockstarsud <sudrockstar123@gmail.com>
2019-04-04 08:41:13 +0530

    Required functionality for populating HTML table using JSON data though AJAX request via Javascript was implemented successfully.

--- TableCreationUsingAjaxJsonJS/script.js
+++ TableCreationUsingAjaxJsonJS/script.js
@@ -3,7 +3,7 @@ const bodyElement = document.querySelector(".books-store > tbody");
 function loadData(){
     const xhttp = new XMLHttpRequest();
 
-    xhttp.open("GET", "table_data.json");
+    xhttp.open("GET", "table_data.json", true);
      
     xhttp.onload = () => {
         try{
@@ -26,7 +26,6 @@ function populateTable(json){
         
         row.forEach((cell) => {
             const td = document.createElement("td");
-      
             td.textContent = cell;
             tr.appendChild(td);
         });
