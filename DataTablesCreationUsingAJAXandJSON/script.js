180de242421d9a96029834aeff5d51deb3d7e78b
Rockstarsud <sudrockstar123@gmail.com>
2019-04-04 19:43:42 +0530

    Updation to the code was done successfully.

--- DataTablesCreationUsingAJAXandJSON/script.js
+++ DataTablesCreationUsingAJAXandJSON/script.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
+
+    // we have added varibale which will be used for DOM/jQuery events.
+    var table = 
     $('.books-store').DataTable({
-        ajax: 'table_data.json'
+
+        // Parsing JSON data to HTML table using AJAX request.
+        ajax: 'table_data.json',
+        
+        // Hides the particular column defined.
+        columnDefs: [
+            
+            // Hiding Office.
+            {
+                "targets": [2],
+                "visible": false,
+                "searchable": false
+            }
+        ],
+
+        // Alternative Pagination: Adds 'First', 'Previous', 'Next' and 'Last' buttons, plus page numbers to your page.
+        "pagingType": "full_numbers"
+
+
     });
+
+    // DOM/jQueryEvent : Gives an alert when clicked on particular row.
+    $('.books-store tbody').on('click', 'tr', function() {
+
+        var data = table.row(this).data();  // retrieves info. about selected row.
+        alert("You are on " + data[0] + "'s row.");
+    });
+
+
 });
