43916c7f55e55711531a6eee8bf9d5d6f12e577f
Rockstarsud <sudrockstar123@gmail.com>
2019-04-05 10:39:30 +0530

    Updation to the code was done successfully.

--- DataTablesCreationUsingAJAXandJSON/script.js
+++ DataTablesCreationUsingAJAXandJSON/script.js
@@ -20,8 +20,6 @@ $(document).ready(function() {
 
         // Alternative Pagination: Adds 'First', 'Previous', 'Next' and 'Last' buttons, plus page numbers to your page.
         "pagingType": "full_numbers"
-
-
     });
 
     // DOM/jQueryEvent : Gives an alert when clicked on particular row.
@@ -30,6 +28,4 @@ $(document).ready(function() {
         var data = table.row(this).data();  // retrieves info. about selected row.
         alert("You are on " + data[0] + "'s row.");
     });
-
-
 });
